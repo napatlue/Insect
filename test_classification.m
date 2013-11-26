07f6bb2ad8e05174867101231d4600532261467b
napatlue <napatlue@gmail.com>
2013-11-25 19:56:13 -0500

    try smome methods

--- test_classification.m
+++ test_classification.m
@@ -8,53 +8,51 @@ function  test_classification()
     size(converted_data,2);
     TEST_class_labels = data.actual_class;
      
-    da = dbaux(4);
+    %da = dbaux(4);
     size(TEST_class_labels,1)
     
-    extracted = extract_feature(converted_data);    
-    for i=1 : length(TEST_class_labels)
+    %extracted = extract_feature(converted_data);    
+    %for i=1 : length(TEST_class_labels)
        % converted_data(i,:) = filter(da,1,converted_data(i,:));
       %t = converted_data(i,:);
-       t = extracted(i,:);
-      %[D1,D2,D3,D4,D5,A1,A2,A3,A4,A5] = wav_extract(t);
-      %D1=abs(D1/norm(D1));
-      %D2=abs(D2/norm(D2));
-%       D3=abs(D3/norm(D3));
-%       D4=abs(D4/norm(D4));
-%       D5=abs(D5/norm(D5));
-%       A1=std(A1/norm(A1));
-%       A2=std(A2/norm(A2));
-%       A3=std(A3/norm(A3));
-%       A4=std(A4/norm(A4));
-%       A5=std(A5/norm(A5)); 
-      
-      %[ca,cb] = dwt(t,'db12');
-      %da = dwt(ca,'db12');
-      %ea = dwt(da,'db12');
-      %fa = dwt(ea,'db12');
-      %[C,L] = wavedec(converted_data(i,:),6,'db12');
-      %ea = normc(ea);
-        
-%       w = 'db12';
-%       [ct,L]=wavedec(t,5,w);
-%       index = (L(1)+1):length(ct);
-%       ct = ct(index);
-      f = fft(t);
-      f = rceps(t); 
-      f = abs(f); % magnitude 
+     %  t = extracted(i,:);
+
+      %f = fft(t);
+      %f = rceps(t); 
+      %f = abs(f); % magnitude 
       
       
-      transform_data(i,:) = f';
+      %transform_data(i,:) = f';
       
       %transform_data(i,:) = [A1 A2 A3 A4 A5];
       %[COEFF,SCORE,latent] = princomp(f);
       %transform_data(i,:) = SCORE;
       
       
-    end
+    %end
     %extracted = extract_feature(transform_data);
     %converted_data = converted_data(1:10,:);
     %TEST_class_labels = data.actual_class(1:10);
+%    transform_data = extract_wavelet_coeff(converted_data,14,'db4');
+ %     for i = 1:size(converted_data ,1)
+ %         %[C(i,:),L] = wavedec(converted_data(i,:),14,'db4');
+ %        [a1,d1] = dwt(converted_data(i,:),'db4');
+ %        [a2,d2] = dwt(a1,'db4');
+ %        [a3,d3] = dwt(a2,'db4');
+ %        transform_data(i,:) = [a2,a3];
+         
+ %     end
+    
+    %coef = keep_feature(C,L,level);
+    
+  %  for i = 1:size(converted_data,1)
+  %      transform_data(i,:) = abs(waverec(C(i,:),L,'haar'));
+  %  end
+  %  for i = 1:size(transform_data,1)
+  %      transform_data(i,:) = abs(fft(transform_data(i,:)));
+  %  end
+    
+    transform_data = converted_data;
     for i=1 : length(TEST_class_labels)
         %classify_object = converted_data(i,:);
         
@@ -69,13 +67,13 @@ function  test_classification()
                 compare_object = transform_data(j,:);
                 %this_distance = Distance_Algorithm(classify_object,compare_object);
                 %this_distance = rand_distance(classify_object,compare_object);
-                this_distance = euclidian_distance(classify_object,compare_object);
+                %this_distance = euclidian_distance(classify_object,compare_object);
                 %this_distance = dtw(classify_object,compare_object);
                 %this_distance = cosine_distance(classify_object,compare_object);
                 
                 %this_distance = sum(abs(compare_object-classify_object)); 
                 %this_distance = sum(abs(compare_object-classify_object)./max(abs(classify_object),0.01));
-                
+                this_distance = 1 - ami(classify_object,compare_object);
                 if(this_distance < best_distance)
                     best_distance = this_distance;
                     predicted_class = TEST_class_labels(j);
